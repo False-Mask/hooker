afb5f37279377b66ab9939cb0038dc2bff9cd5b4
tuzhiqiang <tuzhiqiang@kuaishou.com>
2023-09-15 18:04:37 +0800

    微调 1.添加更多测试用例 2.将AOP切面范围设置到所有Project

--- plugin/hooker/src/main/kotlin/com/dev/tuzhiqiang/agp/HookAsmVisitorFactory.kt
+++ plugin/hooker/src/main/kotlin/com/dev/tuzhiqiang/agp/HookAsmVisitorFactory.kt
@@ -4,6 +4,7 @@ import com.android.build.api.instrumentation.AsmClassVisitorFactory
 import com.android.build.api.instrumentation.ClassContext
 import com.android.build.api.instrumentation.ClassData
 import com.dev.tuzhiqiang.aop.HookClassVisitor
+import com.dev.tuzhiqiang.utils.Logger
 import org.objectweb.asm.ClassVisitor
 
 abstract class HookAsmVisitorFactory: AsmClassVisitorFactory<HookParams> {
@@ -12,6 +13,7 @@ abstract class HookAsmVisitorFactory: AsmClassVisitorFactory<HookParams> {
         classContext: ClassContext,
         nextClassVisitor: ClassVisitor
     ): ClassVisitor {
+        Logger.error(classContext.currentClassData.className)
         return HookClassVisitor(
             classContext,
             nextClassVisitor,
